2926f63c39200f3d8a0d5c9785a38775faedc00b
Ahmedyak1 <ahmedyakine1@gmail.com>
2024-05-27 02:26:55 +0000

    fix task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,26 @@
 #!/usr/bin/node
 
-// Import the 'request' library
+// Import the “request” library
 const request = require('request');
 
-// define constant with the base URL of the Star Wars api
+// Define constant with the base URL of the Star Wars API
 const API_URL = 'https://swapi-api.alx-tools.com/api';
 
 // Check if the number of command line arguments is greater than 2
 if (process.argv.length > 2) {
-  // Make a request to the film resource for the specified film ID
+ // Make a request to the film resource for the specified film ID
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    // If an error occurred during the request, log the error
+ // If an error occurred during the request, log the error
     if (err) {
       console.log(err);
     }
     // Get the characters URL from the film's response body
     const charactersURL = JSON.parse(body).characters;
 
-    // Create an array of Promises that resolve with the names of the characters
+ // Create an array of Promises that resolve with the names of the characters
     const charactersName = charactersURL.map(
       url => new Promise((resolve, reject) => {
-        // Make a request to the character resource
+     // Make a request to the character resource
         request(url, (promiseErr, __, charactersReqBody) => {
           // If an error occurred during the request, reject the Promise with the error
           if (promiseErr) {
@@ -37,5 +37,3 @@ if (process.argv.length > 2) {
       .catch(allErr => console.log(allErr));
   });
 }
-
-
