5b7d12b816a85466111f44527bb9ba24ffb96552
Ahmedyak1 <ahmedyakine1@gmail.com>
2024-05-27 02:35:20 +0000

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,7 +6,7 @@ const request = require('request');
 // Define constant with the base URL of the Star Wars API
 const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-// Check if the number of command line arguments is greater than 2
+// Check if number of command line arguments is greater than 2
 if (process.argv.length > 2) {
  // Make a request to the film resource for the specified film ID
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
